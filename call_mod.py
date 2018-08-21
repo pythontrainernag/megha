5755e2186ab97aa32c1b79b78bc021d40d3db35f
Nageswararao <pythontrainernag@gmail.com>
2018-08-21 07:25:03 +0530

    21/08/2018

---
+++ call_mod.py
@@ -0,0 +1,4 @@
+import mod
+
+print mod.addi(3, 5)
+print mod.subs(4, 6)
---
+++ mod.py
@@ -0,0 +1,11 @@
+'''
+Created on Aug 20, 2018
+
+@author: nexii
+'''
+
+def addi(a, b):
+    return a+b
+
+def subs(a, b):
+    return a-b
